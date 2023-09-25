1b3f36aa066235b5f8a540e71157690cba5835ba
celso-santana <celsojr_ss@hotmail.com>
2023-09-25 19:43:35 -0300

    Commit Inicial - Demopack

---
+++ Demo.Math.Pack/Math/Operations.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Math.Pack.Math
+{
+    public static class Operations
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
